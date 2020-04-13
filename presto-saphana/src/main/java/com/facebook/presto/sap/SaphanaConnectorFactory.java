091f934ea14bc09549872cc471f3713c065a5780
黄聊 <513283439@qq.com>
2020-04-13 15:50:33 +0800

    提交hana

--- presto-saphana/src/main/java/com/facebook/presto/sap/SaphanaConnectorFactory.java
+++ presto-saphana/src/main/java/com/facebook/presto/sap/SaphanaConnectorFactory.java
@@ -27,41 +27,41 @@ import static com.google.common.base.Throwables.throwIfUnchecked;
 import static java.util.Objects.requireNonNull;
 
 public class SaphanaConnectorFactory
-        implements ConnectorFactory
+       // implements ConnectorFactory
 {
-    @Override
-    public String getName()
-    {
-        return "example-hana";
-    }
-
-    @Override
-    public ConnectorHandleResolver getHandleResolver()
-    {
-        return new SaphanaHandleResolver();
-    }
-
-    @Override
-    public Connector create(String catalogName, Map<String, String> requiredConfig, ConnectorContext context)
-    {
-        requireNonNull(requiredConfig, "requiredConfig is null");
-        try {
-            // A plugin is not required to use Guice; it is just very convenient
-            Bootstrap app = new Bootstrap(
-                    new JsonModule(),
-                    new SaphanaModule(catalogName, context.getTypeManager()));
-
-            Injector injector = app
-                    .strictConfig()
-                    .doNotInitializeLogging()
-                    .setRequiredConfigurationProperties(requiredConfig)
-                    .initialize();
-
-            return injector.getInstance(SaphanaConnector.class);
-        }
-        catch (Exception e) {
-            throwIfUnchecked(e);
-            throw new RuntimeException(e);
-        }
-    }
+//    @Override
+//    public String getName()
+//    {
+//        return "example-hana";
+//    }
+//
+//    @Override
+//    public ConnectorHandleResolver getHandleResolver()
+//    {
+//        return new SaphanaHandleResolver();
+//    }
+//
+//    @Override
+//    public Connector create(String catalogName, Map<String, String> requiredConfig, ConnectorContext context)
+//    {
+//        requireNonNull(requiredConfig, "requiredConfig is null");
+//        try {
+//            // A plugin is not required to use Guice; it is just very convenient
+//            Bootstrap app = new Bootstrap(
+//                    new JsonModule(),
+//                    new SaphanaModule(catalogName, context.getTypeManager()));
+//
+//            Injector injector = app
+//                    .strictConfig()
+//                    .doNotInitializeLogging()
+//                    .setRequiredConfigurationProperties(requiredConfig)
+//                    .initialize();
+//
+//            return injector.getInstance(SaphanaConnector.class);
+//        }
+//        catch (Exception e) {
+//            throwIfUnchecked(e);
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
