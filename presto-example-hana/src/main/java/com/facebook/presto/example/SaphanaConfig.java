21e7f721e80e73a03b8758a634aa9dc89e90c5fb
黄聊 <513283439@qq.com>
2020-06-21 16:48:41 +0800

    添加一个模块

---
+++ presto-example-hana/src/main/java/com/facebook/presto/example/SaphanaConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.example;
+
+import com.alibaba.fastjson.JSONObject;
+import com.facebook.airlift.configuration.Config;
+import com.facebook.airlift.log.Logger;
+
+import javax.validation.constraints.NotNull;
+
+import java.net.URI;
+
+public class SaphanaConfig
+{
+    private static final Logger log = Logger.get(SaphanaConfig.class);
+    private URI metadata;
+
+    @NotNull
+    public URI getMetadata()
+    {
+        return metadata;
+    }
+
+    /****
+     * metadata-uri 来自 /etc/catalog/hana.properties中配置项
+     *
+     * @param metadata
+     * @return
+     */
+    @Config("metadata-uri")
+    public SaphanaConfig setMetadata(URI metadata)
+    {
+        log.info("metadata.uri:" + metadata.toString());
+        this.metadata = metadata;
+        return this;
+    }
+}
---
+++ presto-example-hana/src/test/java/com/facebook/presto/example/TestExampleSaphana.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.example;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestExampleSaphana
+{
+
+    private static final String DRIVER = "com.sap.db.jdbc.Driver";
+    private static final String URL = "jdbc:sap://192.168.152.171:39015?currentschema=sics_pod_schema";
+
+    public TestExampleSaphana() {
+    }
+
+    public static void main(String[] args) {
+        TestExampleSaphana demo = new TestExampleSaphana();
+        try {
+            demo.select();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * 查询表字段
+     * @param con
+     * @throws SQLException
+     */
+    public void showColumn(Connection con) throws SQLException{
+        System.out.println("<<<<<<<<<<<<<showColumn>>>>>>>>>>>>>");
+        DatabaseMetaData databaseMetaData = con.getMetaData();
+//    	ResultSet rs = databaseMetaData.getColumns(null, null, "NUMBERS", null);
+        ResultSet rs = databaseMetaData.getColumns(null, "sics_pod_schema".toUpperCase(), "NUMBERS", null);
+        int i = 0;
+        while(rs.next()){
+            String tableName = rs.getString("TABLE_NAME");
+            String columnName = rs.getString("COLUMN_NAME");
+            int dataType = rs.getInt("DATA_TYPE");
+            String dataTypeName = rs.getString("TYPE_NAME");
+            int columnSize = rs.getInt("COLUMN_SIZE");
+            if(tableName.toLowerCase().contains("numbers")){
+                System.out.println(tableName + "," + columnName + "," + dataType + "," + dataTypeName + "," +columnSize );
+            }
+            else{
+                System.out.println(tableName);
+            }
+
+        }
+        System.out.println(">>>>>>>>>>>>>showColumn<<<<<<<<<<<<<");
+    }
+
+    public List<String> getTables(Connection con){
+
+        List<String> list = new ArrayList<>();
+        try {
+            DatabaseMetaData databaseMetaData = con.getMetaData();
+            ResultSet rs = databaseMetaData.getTables(null, "sics_pod_schema".toUpperCase(), null, new String[]{"TABLE"});
+
+            while (rs.next()){
+                String tableName = rs.getString("TABLE_NAME");
+                list.add(tableName);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.print(JSONObject.toJSONString(list));
+        return list;
+    }
+
+    public void select() throws Exception {
+
+        Connection con = this.getConnection(DRIVER, URL, "SYSTEM","sicsPoD2020");
+
+        //查询表字段信息
+        this.showColumn(con);
+        this.getTables(con);
+
+
+        PreparedStatement pstmt = con.prepareStatement("select text, value from sics_pod_schema.numbers");
+        System.out.println(pstmt.getClass().getName());
+        if(pstmt.isWrapperFor(com.sap.db.jdbc.trace.PreparedStatement.class)){
+            System.out.println("true");
+        }
+        ResultSet rs = pstmt.executeQuery();
+        while(rs.next()){
+            String text = rs.getString(1);
+            String value = rs.getString(2);
+            System.out.println(text+","+value);
+        }
+
+        this.closeConnection(con, pstmt);
+
+    }
+
+
+    private Connection getConnection(String driver, String url, String user,
+                                     String password) throws Exception {
+        Class.forName(driver);
+        return DriverManager.getConnection(url, user, password);
+
+    }
+
+    private void closeConnection(Connection con, Statement stmt)
+            throws Exception {
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+}
